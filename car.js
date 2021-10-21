693829e47f5ceed3de045a8ba83ea9306db2fe69
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-21 16:23:52 +0200

    Test 2 parametres

--- car.js
+++ car.js
@@ -39,8 +39,16 @@ class Tv {
     }
 
     volumeDown(num) {
-        this.volume -+ num
-        this.volume < 0 ? this.volume = 0 : this.volume
+        this.volume -= num
+        this.volume <= 0 ? this.volume = 0 : this.volume
+    }
+
+    channelChange(channelUp, channelDown) {
+        this.channel += channelUp
+        this.channel > 50 ? this.channel = this.channel - channelUp : this.channel
+
+        this.channel -= channelDown
+        this.channel <= 0 ? this.channel = this.channel + channelDown : this.channel
     }
 
 
@@ -52,8 +60,11 @@ class Tv {
 let SamsungTv = new Tv ("Samsung", 1, 50)
 
 SamsungTv.volumeDown(63)
+SamsungTv.volumeUp(14)
 
-console.log(SamsungTv)
+SamsungTv.channelChange(24,0)
+SamsungTv.channelChange(0,13)
+
 console.log(SamsungTv)
 
 
