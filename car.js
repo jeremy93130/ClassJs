b37b46f53b2d432d46f1425cab8af19b7d9bddb1
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-21 16:52:23 +0200

    Exercice terminé

--- car.js
+++ car.js
@@ -43,28 +43,39 @@ class Tv {
         this.volume <= 0 ? this.volume = 0 : this.volume
     }
 
-    channelChange(channelUp, channelDown) {
-        this.channel += channelUp
-        this.channel > 50 ? this.channel = this.channel - channelUp : this.channel
+    channelChange(num) {
+        this.channel += num
+        this.channel > 50 ? this.channel = this.channel - num : this.channel
 
-        this.channel -= channelDown
-        this.channel <= 0 ? this.channel = this.channel + channelDown : this.channel
+        this.channel <= 0 ? this.channel = this.channel - num : this.channel
     }
 
+    reset(){
+        this.brand = "Samsung"
+        this.channel = 1
+        this.volume = 50
+    }
 
-
-
-
-
+    discribe(){
+        console.log(`la tv ${this.brand} est actuellement sur le canal ${this.channel} avec le volume à ${this.volume}`)
+    }
 }
-let SamsungTv = new Tv ("Samsung", 1, 50)
 
-SamsungTv.volumeDown(63)
-SamsungTv.volumeUp(14)
+let SamsungTv = new Tv("Samsung", 1, 50)
 
-SamsungTv.channelChange(24,0)
-SamsungTv.channelChange(0,13)
+    SamsungTv.volumeDown(63)
+    SamsungTv.volumeUp(14)
 
-console.log(SamsungTv)
+    SamsungTv.channelChange(25)
+    SamsungTv.channelChange(-11)
+    SamsungTv.channelChange(-20)
+    SamsungTv.reset()
+    SamsungTv.channelChange(20)
+    SamsungTv.volumeUp(52)
+    SamsungTv.discribe()
+
+
+
+    
 
 
